e5ae774214ef20ff5a4295ba8b51d4b95495084e
microvac23 <Ithanmachuca@gmail.com>
2023-06-05 22:36:27 -0500

    code finishing

---
+++ index.js
@@ -0,0 +1,106 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: `What is your GitHub username?`,
+        name: `username`
+    },
+    {
+        type: 'input',
+        message: `What is your GitHub associated email?`,
+        name: `email`
+    },
+    {
+        type: 'input',
+        message: `What is your project's title?`,
+        name: `title`
+    },
+    {
+        type: 'input',
+        message: `What is your project's description?`,
+        name: `description`
+    },
+    {
+        type: 'input',
+        message: `How do you install your product?`,
+        name: `installation`
+    },
+    {
+        type: 'input',
+        message: `What is your project's usage information?`,
+        name: `usage`
+    },
+    {
+        type: 'checkbox',
+        choices: [`a`, `b`, `c`],
+        message: `What is your project's licensing?`,
+        name: `license`
+    },
+    {
+        type: 'input',
+        message: `What is your project's contribution guidelines?`,
+        name: `contribution`
+    },
+    {
+        type: 'input',
+        message: `What is your project's test instructions?`,
+        name: `tests`
+    },
+    
+])
+.then(response => {
+    console.log(response);
+    fs.writeFile('pro.md', `# ${response.title}
+
+## Description
+    
+${response.description}
+    
+## Table of Contents 
+    
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+    
+## Installation
+    
+${response.installation}
+    
+## Usage
+    
+${response.usage}
+    
+## License
+    
+${response.license}
+    
+## Contribute
+    
+${response.contribution}
+    
+## Tests
+    
+${response.tests}
+    
+## Questions
+    
+If you would like to contact me with questions or suggestions you can reach me through...
+    
+Email: ${response.email}
+    
+GitHub: [github.com/${response.username}](https://github.com/${response.username})`, err => {
+        if(err) {
+            console.log(err);
+        }
+    })
+})
+
+function init() {}
+
+// Function call to initialize app
+init();
